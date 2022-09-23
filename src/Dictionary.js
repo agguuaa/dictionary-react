c507316daa14cda519f0efab164eee32b2948744
agguuaa <100287322+agguuaa@users.noreply.github.com>
2022-09-23 16:17:18 +0200

    Added Photos.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
@@ -11,8 +12,12 @@ export default function Dictionary() {
     setKeyword(event.target.value);
   }
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -24,8 +29,12 @@ export default function Dictionary() {
     let pexelsApiKey =
       "563492ad6f91700001000001e7b2800bac8748beaa49fa56b3566ecb";
 
-    let pexelsApiUrl =
-      "https://api.pexels.com/v1/search?query=nature&per_page=1";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: { Authorization: ` Bearer ${pexelsApiKey}` },
+      })
+      .then(handlePexelsResponse);
   }
 
   return (
@@ -36,6 +45,7 @@ export default function Dictionary() {
       </form>
 
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
